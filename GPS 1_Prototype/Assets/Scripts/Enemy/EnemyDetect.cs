1994cebd2572926dd083587494e901725e69663b
KaptainRifleBeard <39362588+KaptainRifleBeard@users.noreply.github.com>
2020-10-14 15:07:31 +0800

    Revert "Stuff"
    This reverts commit 47002594fad61e490ff5c59ef5eef1fecf3b2078, reversing
    changes made to 2e9680899fd09dbe9240ee5024043bd7e68a3828.

--- GPS 1_Prototype/Assets/Scripts/Enemy/EnemyDetect.cs
+++ GPS 1_Prototype/Assets/Scripts/Enemy/EnemyDetect.cs
@@ -19,6 +19,8 @@ public class EnemyDetect : EnemyMovement
     public float attackRadius;
     public float rammingRadius = 2;
 
+    private float rammingCooldown;
+
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;
