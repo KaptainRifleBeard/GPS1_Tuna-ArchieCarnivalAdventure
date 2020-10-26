0b0f017060dce3bf64b4e8311e6e3117e8b112e6
SelinaLim <escales1018@gmail.com>
2020-10-26 22:28:10 +0800

    Added PauseMenu

---
+++ GPS 1_Prototype/Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool pause = false;
+    public GameObject pauseMenu;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(pause)
+            {
+                //resume game
+                pause = false;
+                pauseMenu.SetActive(false);
+                Time.timeScale = 1f;
+            }
+            else
+            {
+                //pause game
+                pause = true;
+                pauseMenu.SetActive(true);
+                Time.timeScale = 0f;  //stop the game
+                
+            }
+
+        }
+        
+    }
+    public void backToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void resume()
+    {
+        //resume game
+        pause = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
