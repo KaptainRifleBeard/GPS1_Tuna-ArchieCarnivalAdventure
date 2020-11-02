d299fcea085cfe8ccb25436d032480b9d239349c
Jonathanhong1999 <71404528+Jonathanhong1999@users.noreply.github.com>
2020-11-02 12:56:11 +0800

    random starting weapon

---
+++ GPS 1_Prototype/Assets/Scripts/Player/RandomWeapon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomWeapon : MonoBehaviour
+{
+    public GameObject[] weapon;
+    public Transform pos;
+    public KeyCode switchWeapon;
+   
+    
+    void Start()
+    {
+        Pick();
+    }
+
+    void Pick()
+    {
+        int randomIndex = Random.Range(0, weapon.Length);
+        GameObject clone = Instantiate(weapon[randomIndex], pos.position, Quaternion.identity);
+
+        
+    }
+    
+    void Update()
+    {
+       
+    }
+}
