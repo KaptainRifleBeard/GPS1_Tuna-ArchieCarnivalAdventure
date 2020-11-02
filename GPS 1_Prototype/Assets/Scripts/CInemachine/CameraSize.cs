cc8c2062f1756a40a5aaaff9cdb96c0781931e2e
SelinaLim <escales1018@gmail.com>
2020-11-02 10:19:55 +0800

    Adjusted Cinemachine camera size

---
+++ GPS 1_Prototype/Assets/Scripts/CInemachine/CameraSize.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSize : MonoBehaviour
+{
+    public Camera cam;
+
+    void Update()
+    {
+        var zoom = 4.4f;
+        Camera.main.orthographicSize = zoom;        
+    }
+
+}
+    
