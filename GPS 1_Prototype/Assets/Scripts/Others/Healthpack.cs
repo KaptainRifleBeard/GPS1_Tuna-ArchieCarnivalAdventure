9163cf8f036f14b9c6367ba5d1549b13611c3a3f
SelinaLim <escales1018@gmail.com>
2020-11-06 12:23:19 +0800

    P2 SHOOT

--- GPS 1_Prototype/Assets/Scripts/Others/Healthpack.cs
+++ GPS 1_Prototype/Assets/Scripts/Others/Healthpack.cs
@@ -18,5 +18,16 @@ public class Healthpack : MonoBehaviour
             Destroy(gameObject);
 
         }
+
+
+        PlayerB playerB = collision.GetComponent<PlayerB>();
+        if (playerB != null)
+        {
+            Debug.Log("i hit player ady!!!!!!!!!!!");
+            playerB.heal(healAmount);
+            addHealth = true;
+            Destroy(gameObject);
+
+        }
     }
 }
