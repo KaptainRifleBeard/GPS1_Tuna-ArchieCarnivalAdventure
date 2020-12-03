251dee68d93aff7d0e7852d3310aa7a4ff21eb72
SelinaLim <escales1018@gmail.com>
2020-12-04 03:59:51 +0800

    Added button select
    SPACE to select
    UP DOWN LEFT RIGHT to move

---
+++ GPS 1_Prototype/Assets/Scripts/Others/ButtonScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour
+{
+    public string key;
+    public void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            //for pc, Return = Enter Button
+            EventSystem.current.SetSelectedGameObject(this.gameObject);
+        }
+    }
+  
+
+}
+    
