a3ef1ba2b32404c43749b61fcd90eda7f493651f
KaptainRifleBeard <39362588+KaptainRifleBeard@users.noreply.github.com>
2020-12-04 13:33:53 +0800

    Messed with Health Pack Sprite Randomness

--- GPS 1_Prototype/Assets/Scripts/Enemy/EnemyHealth.cs
+++ GPS 1_Prototype/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -47,16 +47,16 @@ public class EnemyHealth : MonoBehaviour
     {
         int randNum = Random.Range(0, 100); // 100% total for determining loot chance;;
 
-        if (randNum > 0 && randNum <= 15) 
+        if (randNum > 0 && randNum <= 13) 
         {
             Instantiate(dropItems[0], gameObject.transform.position, Quaternion.identity);
         }
-        if (randNum > 16 && randNum <= 30)
+        if (randNum > 14 && randNum <= 23)
         {
             Instantiate(dropItems[1], gameObject.transform.position, Quaternion.identity);
 
         }
-        if (randNum > 31 && randNum <= 50)
+        if (randNum > 24 && randNum <= 40)
         {
             Instantiate(dropItems[2], gameObject.transform.position, Quaternion.identity);
 
